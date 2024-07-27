837ec0638f8499e41762fb45dceae9c8563c2afd
mukeshutmani <kumarmukesh789123@gmail.com>
2024-07-27 20:55:12 +0500

    add hacker rank problem no 11

---
+++ Main.java
@@ -0,0 +1,60 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String[] args) {
+     Scanner scn = new Scanner(System.in); 
+
+     String sentence = scn.nextLine();
+        sentence = sentence.toLowerCase();
+        
+     boolean pangram = isPangram(sentence);
+       
+     if(pangram){
+             System.out.println("pangram");
+     } else {
+        System.out.println("not pangram");
+     } 
+
+
+    
+}
+
+public static boolean isPangram(String sentence){
+
+    boolean[] letterPresent = new boolean[26];
+
+
+    for(int i=0; i<sentence.length(); i++){
+       char ch = sentence.charAt(i);
+       if(ch>='a' && ch<='z'){
+        letterPresent[ch - 'a'] = true;
+       }
+    }
+
+    for(boolean present:letterPresent){
+        if(!present){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
